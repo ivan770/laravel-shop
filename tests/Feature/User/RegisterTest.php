0c29a0d0f3a7d990c701679de2930cb8fa378469
ivan770 <leshenko.ivan770@gmail.com>
2019-05-15 21:10:28 +0300

    Registration test

---
+++ tests/Feature/User/RegisterTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Feature\User;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RegisterTest extends TestCase
+{
+    use WithFaker;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+    protected function logout()
+    {
+        return $this->post('/logout');
+    }
+
+    protected function sendRegisterData($name = null, $email = null, $password = null)
+    {
+        return $this->json('POST', '/register', [
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+            'password_confirmation' => $password
+        ]);
+    }
+
+    public function testValidData()
+    {
+        $name = $this->faker->name;
+        $email = $this->faker->email;
+        $password = $this->faker->password;
+        $this->sendRegisterData($name, $email, $password);
+        $this->assertAuthenticated();
+    }
+
+    public function testShortPassword()
+    {
+        $name = $this->faker->name;
+        $email = $this->faker->email;
+        $password = $this->faker->password(2, 4);
+        $response = $this->sendRegisterData($name, $email, $password);
+        $response->assertJsonValidationErrors(['password']);
+    }
+
+    public function testMissingName()
+    {
+        $email = $this->faker->email;
+        $password = $this->faker->password;
+        $response = $this->sendRegisterData(null, $email, $password);
+        $response->assertJsonValidationErrors(['name']);
+    }
+
+    public function testMissingEmail()
+    {
+        $name = $this->faker->name;
+        $password = $this->faker->password;
+        $response = $this->sendRegisterData($name, null, $password);
+        $response->assertJsonValidationErrors(['email']);
+    }
+
+    public function testExistingEmail()
+    {
+        $password = $this->faker->password;
+        $email = $this->faker->email;
+        $this->sendRegisterData($email, $password);
+        $this->logout();
+        $response = $this->sendRegisterData($email, $password);
+        $response->assertJsonValidationErrors(['email']);
+    }
+
+    public function testMissingPassword()
+    {
+        $name = $this->faker->name;
+        $email = $this->faker->email;
+        $response = $this->sendRegisterData($name, $email, null);
+        $response->assertJsonValidationErrors(['password']);
+    }
+}
