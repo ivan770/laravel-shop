b2d10897c920b60d5e598a2041ac1e4f3a986574
ivan770 <leshenko.ivan770@gmail.com>
2019-05-15 20:46:41 +0300

    Addresses in Nova

--- app/Nova/Address.php
+++ app/Nova/Address.php
@@ -2,29 +2,28 @@
 
 namespace App\Nova;
 
-use Laravel\Nova\Fields\BelongsToMany;
-use Laravel\Nova\Fields\HasMany;
+use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\ID;
 use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Text;
-use Laravel\Nova\Fields\Gravatar;
-use Laravel\Nova\Fields\Password;
+use Laravel\Nova\Http\Requests\NovaRequest;
 
-class User extends Resource
+class Address extends Resource
 {
     /**
      * The model the resource corresponds to.
      *
      * @var string
      */
-    public static $model = 'App\\Models\\User';
+    public static $model = 'App\\Models\\Address';
 
     /**
      * The single value that should be used to represent the resource when being displayed.
      *
      * @var string
      */
-    public static $title = 'name';
+    public static $title = 'id';
 
     /**
      * The columns that should be searched.
@@ -32,7 +31,7 @@ class User extends Resource
      * @var array
      */
     public static $search = [
-        'id', 'name', 'email',
+        'id', 'region', 'city', 'street'
     ];
 
     /**
@@ -45,26 +44,12 @@ class User extends Resource
     {
         return [
             ID::make()->sortable(),
-
-            Gravatar::make(),
-
-            Text::make('Name')
-                ->sortable()
-                ->rules('required', 'max:255'),
-
-            Text::make('Email')
-                ->sortable()
-                ->rules('required', 'email', 'max:254')
-                ->creationRules('unique:users,email')
-                ->updateRules('unique:users,email,{{resourceId}}'),
-
-            Password::make('Password')
-                ->onlyOnForms()
-                ->creationRules('required', 'string', 'min:6')
-                ->updateRules('nullable', 'string', 'min:6'),
-
-            HasMany::make('Carts'),
-            HasMany::make('Addresses')
+            Text::make('Region'),
+            Text::make('City'),
+            Text::make('Street'),
+            Number::make('Building'),
+            Number::make('Apartment')->nullable(),
+            BelongsTo::make('User')
         ];
     }
 
@@ -76,7 +61,7 @@ class User extends Resource
      */
     public function cards(Request $request)
     {
-        return [new Metrics\NewUsers, new Metrics\UsersPerDay];
+        return [];
     }
 
     /**
