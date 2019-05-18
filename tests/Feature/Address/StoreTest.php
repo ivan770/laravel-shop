3f6e010f0916b27c763e273235ad2f81de50548a
ivan770 <leshenko.ivan770@gmail.com>
2019-05-18 11:23:14 +0300

    Address store test

---
+++ tests/Feature/Address/StoreTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature\Address;
+
+use Laravel\Passport\Passport;
+
+class StoreTest extends BaseAddress
+{
+    protected function requestStore($data)
+    {
+        return $this->json("POST", "/api/address", $data);
+    }
+
+    protected function getData()
+    {
+        return [
+            'region' => $this->faker->state,
+            'city' => $this->faker->city,
+            'street' => $this->faker->streetAddress,
+            'building' => $this->faker->buildingNumber,
+            'apartment' => $this->faker->randomDigitNotNull
+        ];
+    }
+
+    public function testValidStore()
+    {
+        Passport::actingAs($this->user, ['address']);
+        $response = $this->requestStore($this->getData());
+        $response->assertJsonStructure([
+            "data" => [
+                "region", "city", "street", "building", "apartment"
+            ]
+        ]);
+    }
+
+    public function testUnauthenticated()
+    {
+        $response = $this->requestStore($this->getData());
+        $response->assertStatus(401);
+    }
+}
