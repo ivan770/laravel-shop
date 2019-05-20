26502d0881b56edf48814dc35cdfab58993b8537
ivan770 <leshenko.ivan770@gmail.com>
2019-05-20 17:32:29 +0300

    Ignore accepted exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -13,7 +13,8 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        \Stripe\Error\Card::class,
+        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
     ];
 
     /**
